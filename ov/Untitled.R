2a84828e3c0164832f7de834bd711214f4163372
wangyouquan <wangyouquan@qutoutiao.net>
2020-10-23 23:03:26 +0800

    test2

---
+++ ov/Untitled.R
@@ -0,0 +1,7 @@
+#this is a tset 
+
+setwd("/Users/admin/Desktop/ov/WomenDisease/ov")           #???ù???Ŀ¼
+inputFile="symbol.txt"                                                  #?????ļ?????
+
+#??ȡ?ļ?
+rt=read.table(inputFile,sep="\t",header=T,check.names=F)
